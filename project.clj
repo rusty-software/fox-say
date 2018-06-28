ca2c348d9d3a3965ffcdab7020797e7f307ca1b6
Rusty Bentley <mycoding.alert@gmail.com>
2018-06-28 08:16:43 -0500

    adds reagent to project

--- project.clj
+++ project.clj
@@ -4,18 +4,19 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  
-  
   :min-lein-version "2.7.1"
 
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.10.238"]
-                 [org.clojure/core.async  "0.4.474"]]
+                 [org.clojure/core.async  "0.4.474"]
+                 [reagent "0.8.1"]]
 
   :plugins [[lein-figwheel "0.5.16"]
             [lein-cljsbuild "1.1.7" :exclusions [[org.clojure/clojure]]]]
 
   :source-paths ["src"]
+
+  :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"]
 
   :cljsbuild {:builds
               [{:id "dev"
@@ -31,7 +32,7 @@
                            ;; Comment this out once it no longer serves you.
                            :open-urls ["http://localhost:3449/index.html"]}
 
-                :compiler {:main fox-say.core
+                :compiler {:main fox-say.main
                            :asset-path "js/compiled/out"
                            :output-to "resources/public/js/compiled/fox_say.js"
                            :output-dir "resources/public/js/compiled/out"
@@ -45,7 +46,7 @@
                {:id "min"
                 :source-paths ["src"]
                 :compiler {:output-to "resources/public/js/compiled/fox_say.js"
-                           :main fox-say.core
+                           :main fox-say.main
                            :optimizations :advanced
                            :pretty-print false}}]}
 
